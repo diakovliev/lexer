158eb88c604487caa7101b74587c74c4f0641baf
dmytro.iakovliev <Iakovliev.D@eplan.de>
2024-09-28 12:33:17 +0300

    Add csv example

---
+++ examples/csv2json/csv2json.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/diakovliev/lexer/examples/csv2json/csv"
+)
+
+var (
+	inputFile  string
+	outputFile string
+	separator  string
+	withHeader bool
+)
+
+func init() {
+	flag.StringVar(&inputFile, "i", "", "Input file, if no - stdin will be used.")
+	flag.StringVar(&outputFile, "o", "", "Output file, if no - stdout will be used.")
+	flag.StringVar(&separator, "s", ",", "Separator, ',' by default.")
+	flag.BoolVar(&withHeader, "wh", false, "Treat the first line as a header with column names.")
+}
+
+func main() {
+	flag.Parse()
+	input := os.Stdin
+	var err error
+	if inputFile != "" {
+		input, err = os.OpenFile(inputFile, os.O_RDONLY, 0o644)
+		if err != nil {
+			fmt.Printf("ERROR: %s\n", err)
+			return
+		}
+	}
+	output := os.Stdout
+	if outputFile != "" {
+		output, err = os.OpenFile(outputFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+		if err != nil {
+			fmt.Printf("ERROR: %s\n", err)
+			return
+		}
+	}
+	err = csv.Do(input, output, separator[0], withHeader)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		return
+	}
+}
